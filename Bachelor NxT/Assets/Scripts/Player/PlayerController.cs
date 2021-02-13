562b5d274bb803461f8c6b330c19301946ad05a4
Stef Kluskens <kluskens_stef@hotmail.com>
2021-02-13 10:19:09 +0100

    Added TMP + LeanTween + Pedestrian Scripts

---
+++ Bachelor NxT/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float mouseSensitivity = 3.5f;
+    [SerializeField] private float walkSpeed = 6.0f;
+    [SerializeField] private float jumpHeight = 5.0f;
+    [SerializeField] private float gravity = -13.0f;
+    [SerializeField] [Range(0.0f, 0.5f)] private float moveSmoothTime = 0.3f;
+    [SerializeField] [Range(0.0f, 0.5f)] private float mouseSmoothTime = 0.03f;
+
+    private float cameraPitch = 0.0f;
+    private float velocityY = 0.0f;
+    private bool lockCursor = true;
+    private CharacterController characterController;
+
+    private Vector2 currentDirection = Vector2.zero;
+    private Vector2 currentDirectionVelocity = Vector2.zero;
+    private Vector2 currentMouseDelta = Vector2.zero;
+    private Vector2 currentMouseDeltaVelocity = Vector2.zero;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseLook();
+        UpdateMovement();
+    }
+
+    public static void LockMouse(bool shouldCursorBeLocked) //use to lock or unlock cursor
+    {
+        if (shouldCursorBeLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void UpdateMouseLook()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        //smooth mouse movement
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+        transform.Rotate(Vector3.up, currentMouseDelta.x * mouseSensitivity);
+    }
+
+    private void UpdateMovement()
+    {
+        Vector2 targetDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDirection.Normalize();
+        //smooth movement
+        currentDirection = Vector2.SmoothDamp(currentDirection, targetDirection, ref currentDirectionVelocity, moveSmoothTime);
+
+        if (characterController.isGrounded)
+        {
+            velocityY = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        {
+            velocityY += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        velocityY += gravity * Time.deltaTime;
+        Vector3 velocity = (transform.forward * currentDirection.y + transform.right * currentDirection.x) * walkSpeed + Vector3.up * velocityY;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
